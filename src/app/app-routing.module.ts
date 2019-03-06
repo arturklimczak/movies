d436021c5f5a12a043e53a0b479a829872596d28
Artur Klimczak <artur.r.klimczak@gmail.com>
2019-03-06 15:14:42 +0100

    works

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,10 @@ import { Routes, RouterModule } from '@angular/router';
 import { routes } from './app-routes';
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,
+    {
+        useHash: true
+    })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
