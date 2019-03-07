f17383ab63297983b8478bc9b5befb9c8d0dbc93
Artur Klimczak <artur.r.klimczak@gmail.com>
2019-03-07 10:17:23 +0100

    change detector

--- src/app/components/movie-list-item/movie-list-item.component.ts
+++ src/app/components/movie-list-item/movie-list-item.component.ts
@@ -1,23 +1,34 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-movie-list-item',
   templateUrl: './movie-list-item.component.html',
-  styleUrls: ['./movie-list-item.component.css']
+  styleUrls: ['./movie-list-item.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class MovieListItemComponent implements OnInit {
   @Input() movie = null;
 
   constructor(
-    private router: Router
+    private router: Router,
+    private changeDetector: ChangeDetectorRef
   ) { }
 
   ngOnInit() {
+    setTimeout(() => {
+      this.movie.rate = 10;
+      this.changeDetector.markForCheck();
+    }, 2000)
   }
 
   redirectMovieProfile() {
     this.router.navigateByUrl('/movies/' + this.movie.id);
+  }
+
+  buildRate() {
+    console.log(1);
+    return this.movie.rate * 10 + '%';
   }
 
 }
