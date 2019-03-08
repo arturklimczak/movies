df748fb73a197f1c6b21f4f703e89f4bb5c306cb
Artur Klimczak <artur.r.klimczak@gmail.com>
2019-03-08 08:55:47 +0100

    event emitter

--- src/app/core/components/video-player/video-player.component.ts
+++ src/app/core/components/video-player/video-player.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, ElementRef } from '@angular/core';
+import { Component, OnInit, Input, ElementRef, Output, EventEmitter, HostBinding, ViewChild } from '@angular/core';
 import { Movie } from 'src/app/core/interfaces/movie.interface';
 
 @Component({
@@ -7,13 +7,26 @@ import { Movie } from 'src/app/core/interfaces/movie.interface';
   styleUrls: ['./video-player.component.css']
 })
 export class VideoPlayerComponent implements OnInit {
-  movieUrl = 'https://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4';
 
   @Input() movie: Movie = null;
+  @Output() status = new EventEmitter();
+  @ViewChild('player') player = null;
+
+  movieUrl = 'https://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4';
 
   constructor(private $element: ElementRef) { }
 
   ngOnInit() {
+  }
+
+  play() {
+    this.status.next({ status: 'playing'});
+    this.player.nativeElement.play();
+  }
+
+  pause() {
+    this.status.next({ status: 'paused'});
+    this.player.nativeElement.pause();
   }
 
   fullscreen() {
--- src/app/core/pages/page-not-found/page-not-found.component.ts
+++ src/app/core/pages/page-not-found/page-not-found.component.ts
@@ -6,10 +6,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./page-not-found.component.css']
 })
 export class PageNotFoundComponent implements OnInit {
+  playerStatus: string = 'loading';
+
+  movie = {
+    "id": "uC-_Gon2p9M",
+    "title": "Green Book",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vulputate sit amet odio ac porttitor. Cras facilisis magna volutpat erat mollis, a malesuada ante rutrum. Ut eget ex non dui ullamcorper vulputate. Mauris metus risus, consequat in metus at, volutpat mattis est.",
+    "thumb_url": "http://i3.ytimg.com/vi/uC-_Gon2p9M/hqdefault.jpg",
+    "movie_url": "https://www.youtube.com/embed/uC-_Gon2p9M",
+
+    "year": 2017,
+    "duration": 99,
+    "rate": 10
+  };
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  handleStatus(event) {
+    console.log(event);
+    switch (event.status) {
+      case 'playing':
+        this.playerStatus = 'Player is playing';
+        break;
+      case 'paused':
+        this.playerStatus = 'Player is paused';
+        break;
+      default:
+        this.playerStatus = 'Player is idle';
+    }
   }
 
 }
