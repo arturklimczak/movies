6311594a43cdb87329c7a977ef5ad249b4694c3a
Artur Klimczak <artur.r.klimczak@gmail.com>
2019-03-08 15:03:26 +0100

    pipe test

--- src/app/core/services/movies.service.spec.ts
+++ src/app/core/services/movies.service.spec.ts
@@ -1,15 +1,48 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, flush, fakeAsync } from '@angular/core/testing';
 
 import { MoviesService } from './movies.service';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('MoviesService', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    imports: [ HttpClientTestingModule]
-  }));
+  let service: MoviesService = null;
+
+  beforeEach(() => {
+      TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule
+      ]
+    });
+
+    service = TestBed.get(MoviesService);
+  });
 
   it('should be created', () => {
     const service: MoviesService = TestBed.get(MoviesService);
     expect(service).toBeTruthy();
+  });
+
+  describe('fetchMovies', () => {
+    it('should exists', () => {
+      expect(service.fetchMovies).toEqual(jasmine.any(Function));
+    });
+
+    xit('should return long list of movies', () => {
+      service.fetchMovies()
+        .then(movies => {
+          expect(movies.length).toEqual(4);
+        })
+    });
+  });
+
+  describe('fetchMovies', () => {
+    it('', () => {
+
+    });
+  });
+
+  describe('fetchMovies', () => {
+    it('', () => {
+
+    });
   });
 });
